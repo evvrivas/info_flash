fbe78bb2c4cd34d09f87fe404ee01a3c961425ea
evvrivas <evvrivas@gmail.com>
2019-01-07 19:45:55 -0600

    guau

--- mysite/views.py
+++ mysite/views.py
@@ -415,7 +415,6 @@ def tabular_datos(request):
 
 def grafico_principal(request):   
 
-        datos_barras=Datos_a_graficar.objects.all().last()
         datos=Datos_a_graficar.objects.all()
 
         datosfml=datos.values_list("fml", flat=True)
@@ -440,34 +439,15 @@ def grafico_principal(request):
                
         #barh(pos,datos,align = 'center')
         f=plt.figure()
-        f, axs = plt.subplots(2, 1, figsize=(9, 9), sharey=True)
-        axs[0].plot(X,Y1, 'r')
-        axs[0].plot(X,Y2, 'c')
-        axs[0].plot(X,Y3, 'b')
-        axs[0].plot(X,Y4, 'p')
-        axs[0].plot(X,Y5, 'b')
-        axs[0].plot(X,Y6, 'g')
-        axs[0].plot(X,Y7, 'o')  
+        plt.plot(X,Y1, 'r')
+        plt.plot(X,Y2, 'c')
+        plt.plot(X,Y3, 'b')
+        plt.plot(X,Y4, 'p')
+        plt.plot(X,Y5, 'b')
+        plt.plot(X,Y6, 'g')
+        plt.plot(X,Y7, 'o')   
 
-        axs[0].xlabel('Datos de prueba ')
-        axs[0].ylabel('PREFERENCIAS')
-        titulo="Tendencia del las preferencias"
-        axs[0].plt.title(titulo)      
-
-
-        x=["fml","gan","vamo","alianza","aren","pc","pd","dsv"]
-      
-        Y1 = np.asarray(datos_barras)
-        axs[1].bar(x, Y1)
-        axs[1].xlabel('Estudiados')
-        axs[1].ylabel('PREFERENCIAS')
-        titulo="Grafico de barras"
-        axs[1].plt.title(titulo)      
-
-
-
-      
-        plt.grid()     
+        ax.grid()     
           
         plt.xlabel('Datos de prueba ')
         plt.ylabel('PREFERENCIAS')
@@ -526,15 +506,13 @@ def calculo_de_datos():
 
 
 
-        try:
-            datos=Datos_a_graficar.objects.all().last() 
-                      
-
-            for i in range(len(datos)):
+        
+        datos=Datos_a_graficar.objects.all().last() 
+        finales=[]            
+        for i in range(len(datos_a_sumar)):
                 suma=datos_a_sumar[i]+datos[i]
-                finales[i]=suma
-        except:
-            finales=datos_a_sumar
+                finales.append(suma)
+      
 
 
         Cuestionario_temporal.objects.all().delete()
